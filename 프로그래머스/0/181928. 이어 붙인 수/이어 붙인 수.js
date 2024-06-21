4f51529f37d551232daf699f75350a013df2a85c
정혜인 <110520124+zelkovaria@users.noreply.github.com>
2024-06-21 17:47:11 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -3,5 +3,5 @@ function solution(num_list) {
     let even = [];
     
     num_list.filter((num) => num % 2 === 0 ? even.push(num) : odd.push(num));
-    return Number(odd.join('')) + Number(even.join(''));
+    return +odd.join('') + +even.join('');
 }
