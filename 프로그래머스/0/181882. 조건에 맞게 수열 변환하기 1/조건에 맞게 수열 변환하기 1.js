9cf1c0f195c1b2d086025762fa1b72e998a5b330
정혜인 <110520124+zelkovaria@users.noreply.github.com>
2024-07-09 00:47:58 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 20.24 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    return arr.map(num => {
+        if (num >= 50 && num % 2 === 0) {
+            return num/2;
+        } else if (num < 50 && num % 2 !== 0) {
+            return num * 2;
+        } else return num;
+    })
+}
