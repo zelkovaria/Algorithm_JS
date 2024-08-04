6a2129415f6aa0293028edbb43dc86c337c0dcd7
정혜인 <110520124+zelkovaria@users.noreply.github.com>
2024-08-04 11:29:56 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,12 @@
+function solution(date1, date2) {
+     if (date1[0] < date2[0]) return 1;
+    else if (date1[0] > date2[0]) return 0;
+
+    if (date1[1] < date2[1]) return 1;
+    else if (date1[1] > date2[1]) return 0;
+
+    if (date1[2] < date2[2]) return 1;
+    else if (date1[2] > date2[2]) return 0;
+
+    return 0;
+}
