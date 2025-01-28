991d1340af6e7f72e08c84e29518881d6d06be73
정혜인 <110520124+zelkovaria@users.noreply.github.com>
2025-01-28 23:36:51 +0900

    Time: 1 ms (61.51%), Space: 51.4 MB (66.09%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let score = 0;
+
+      for (let i = 0; i < s.length - 1; i++) {
+        const diff = Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1)); 
+        score += diff;
+    }
+
+    return score;
+};
