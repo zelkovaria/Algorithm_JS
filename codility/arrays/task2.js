e9d12f28d5328b67355f0bd5f95c29cb2c820379
zelkovaria <hyein396@gmail.com>
2025-03-27 21:45:33 +0900

    codility Arrays-task2 js

---
+++ codility/arrays/task2.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var getSmallestString = function (s) {
+  let best = s;
+  const arr = s.split("");
+
+  for (let i = 0; i < s.length - 1; i++) {
+    const leftNum = parseInt(s[i], 10);
+    const rightNum = parseInt(s[i + 1], 10);
+
+    if (leftNum % 2 === rightNum % 2) {
+      let candidateArr = arr.slice();
+      [candidateArr[i], candidateArr[i + 1]] = [
+        candidateArr[i + 1],
+        candidateArr[i],
+      ];
+      const candidate = candidateArr.join("");
+
+      if (candidate < best) best = candidate;
+    }
+  }
+
+  return best;
+};
