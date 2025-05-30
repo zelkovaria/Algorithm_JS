b1c122d4a4728d12c3a580fefa235bf34e2bf862
정혜인 <110520124+zelkovaria@users.noreply.github.com>
2025-05-30 21:18:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,9 @@
+def solution(cards1, cards2, goal):
+    for word in goal:
+        if cards1 and cards1[0] == word:
+            cards1.pop(0)
+        elif cards2 and cards2[0] == word:
+            cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
