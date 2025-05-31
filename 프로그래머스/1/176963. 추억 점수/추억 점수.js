fe9480627feee1526174d68e19b5764644a7fb17
정혜인 <110520124+zelkovaria@users.noreply.github.com>
2025-05-31 11:49:43 +0900

    [level 1] Title: 추억 점수, Time: 3.10 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,28 +1,22 @@
 function solution(name, yearning, photo) {
-    let namesValue = matchScores(name, yearning);
-    return findPhoto(namesValue, photo);
-}
-
-function matchScores(name, yearning) {
-    let namesValue = name.map((name, index) => 
-        yearning[index] !== undefined ? [name, yearning[index]] : [name, 0]);
+    const yearningByName = {}
+    const answer = []
     
-    return namesValue;
-}
-
-function findPhoto(namesValue, photo) {
-    let photoScore = [];
-    
-    for (let i = 0; i < photo.length; i++) {
-        let sumScore = 0; 
-        for (let j = 0; j < photo[i].length; j++) {
-            const nameIndex = namesValue.findIndex(pair => pair[0] === photo[i][j]);
-            if (nameIndex !== -1) {
-                sumScore += namesValue[nameIndex][1];
-            }
-        }
-        photoScore.push(sumScore);
+    for (let i = 0; i < name.length; i++) {
+        yearningByName[name[i]] = yearning[i]
     }
     
-    return photoScore;
+    for (let i = 0; i < photo.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < photo[i].length; j++) {
+            if (yearningByName[photo[i][j]]) {
+                sum += yearningByName[photo[i][j]];
+            } else {
+                sum += 0;
+            }
+        }
+        answer.push(sum);
+    }
+    
+    return answer;
 }
