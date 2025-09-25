404ae4f020636183a85c3d166443bb1f4a3877cb
정혜인 <110520124+zelkovaria@users.noreply.github.com>
2025-09-25 23:47:00 +0900

    [Silver I] Title: 절댓값 힙, Time: 276 ms, Memory: 29148 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.js
@@ -0,0 +1,94 @@
+class MinHeap {
+  constructor() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  push(value) {
+    this.items.push(value);
+    this._bubbleUp(this.items.length - 1);
+  }
+
+  pop() {
+    if (this.size() === 0) return 0;
+    if (this.size() === 1) return this.items.pop();
+
+    const min = this.items[0];
+    this.items[0] = this.items.pop();
+    this._bubbleDown(0);
+    return min;
+  }
+
+  _bubbleUp(index) {
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this._compare(this.items[index], this.items[parentIndex]) >= 0) break;
+      this._swap(parentIndex, index);
+      index = parentIndex;
+    }
+  }
+
+  _bubbleDown(index) {
+    const n = this.size();
+    while (true) {
+      const left = index * 2 + 1;
+      const right = index * 2 + 2;
+      let smallest = index;
+
+      if (
+        left < n &&
+        this._compare(this.items[left], this.items[smallest]) < 0
+      ) {
+        smallest = left;
+      }
+      if (
+        right < n &&
+        this._compare(this.items[right], this.items[smallest]) < 0
+      ) {
+        smallest = right;
+      }
+      if (smallest === index) break;
+
+      [this.items[index], this.items[smallest]] = [
+        this.items[smallest],
+        this.items[index],
+      ];
+      index = smallest;
+    }
+  }
+
+  _swap(i, j) {
+    [this.items[i], this.items[j]] = [this.items[j], this.items[i]];
+  }
+
+  _compare(a, b) {
+    if (Math.abs(a) === Math.abs(b)) return a - b;
+    return Math.abs(a) - Math.abs(b);
+  }
+}
+
+function solution(input) {
+  const N = Number(input[0]);
+  const heap = new MinHeap();
+  const out = [];
+
+  for (let i = 1; i <= N; i++) {
+    const x = Number(input[i]);
+    if (x === 0) out.push(heap.pop());
+    else heap.push(x);
+  }
+
+  console.log(out.join("\n"));
+}
+
+const input = [];
+require("readline")
+  .createInterface({ input: process.stdin, output: process.stdout })
+  .on("line", (line) => input.push(line))
+  .on("close", () => {
+    solution(input);
+    process.exit();
+  });
